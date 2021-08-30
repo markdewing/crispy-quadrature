0eda376683ddc1ddfbace8aaafeaa8584c2adf83
Mark Dewing <markdewing@gmail.com>
2021-08-30 15:00:43 -0500

    Change definition of n in CC quadrature
    Was the number of intervals and matched the defintion of N from the
    wikipedia page.
    
    Makes more sense to define n as the number of interior grid points,
    since that's the number of function evaluations necessary.

--- python/trap1_cc.py
+++ python/trap1_cc.py
@@ -1,16 +1,18 @@
 
 from math import exp, pi, tan, sin, sqrt
 
-# Clenshaw-Curtis - trapezoidal rule from -oo to oo with n divisions
+# Clenshaw-Curtis - trapezoidal rule from -oo to oo with n points
 # https://en.wikipedia.org/wiki/Clenshaw%E2%80%93Curtis_quadrature
 
 # Coordinate transformation
 def transform_cc(t):
+    # Problem-dependent convergence parameter
     L = 1.0
     return L/tan(t)
 
 # Jacobian of the transformation
 def jacobian_cc(t):
+    # Problem-dependent convergence parameter
     L = 1.0
     return L/sin(t)**2
 
@@ -18,11 +20,12 @@ def trap_cc(n, f1):
     a = 0.0
     b = pi
     # Size of each interval
-    h = (1.0/n)*(pi)
+    h = (1.0/(n+1))*(pi)
 
     total = 0.0
 
-    for i in range(1,n):
+    for i in range(1, n+1):
+        # grid points in (0,pi)
         x =  h*i
         total += h*f1(transform_cc(x))*jacobian_cc(x)
 
@@ -36,8 +39,13 @@ def exp_f(x):
 if __name__ == '__main__':
     a = 0.0
     b = pi
-    n = 40
 
+
+    # We assume the end points (integrand at infinity) evaluate to zero,
+    # so only the interior points are used.
+
+    # Number of interior grid points.
+    n = 10
 
     print('Expected value: ',sqrt(pi))
     # ---------------------
